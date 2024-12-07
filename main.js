7f193fa514a40a8e38e5af4988f072a3c8e16846
karlhenriksson <153213827+karlhenriksson@users.noreply.github.com>
2024-12-07 13:18:19 +0100

    Uppdaterade, lade till personer

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 "use strict";
 /**
  * Hanterar secret-santa-grejer
+ *
+ * VARNING: Man kan se allas secret santa i den här filen! Det är inte mycket jag
+ * kan göra åt det tyvärr, så jag får helt enkelt be snällt att du inte läser vidare
+ * i filen.
  */
 
 const bg = document.getElementById("background");
@@ -11,7 +15,7 @@ async function handleSnowflakes() {
   const w = document.body.offsetWidth;
   const h = document.body.offsetHeight;
   const x = Math.random() * (w - 100);
-  const y = Math.random() * (h - (w < 800 ? 50 : 150));
+  const y = Math.random() * (h - (w < 800 ? 50 : 200));
 
   addSnowflake(x, y);
 
@@ -54,7 +58,6 @@ function revealName() {
 
   // Gå igenom regex-dictionaryn
   for (const [key, value] of Object.entries(prompts)) {
-    console.log(key);
     if (new RegExp(key, "i").test(string)) {
       const out = value.replace(
         "$",
@@ -76,11 +79,37 @@ function output(string) {
 }
 
 // Alla prompts för programmet
+// LÄS INTE DENNA OM DU INTE VILL SPOILA FÖR ANDRA!!!
 const prompts = {
+  "^arvid$":
+    "Skriv första bokstaven på ditt efternamn också så att du inte råkar se fel person.",
+  "^1234?5?6?7?8?9?$": "Vad är roten till x^2 - 16x + 64?",
+  "^(8|åtta)$": "Att vara eller inte vara, det är [?]",
+  "frågan??": "Frågan är, hur många dagar har Februari nästa år?",
+  "28 *(|st|dagar)":
+    '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">https://forms.gle/Eg8sXi2f=</a>',
+  bånx: "Testa med O i stället.",
+  bonx: "Yay!",
   // ^Förnamn *(e(fternamn)?)? *$
-  "^tove *(l(eander)?)? *(r(önngren)?)? *$":
-    "$ Karl, Malin, Bonx, Bånx och påven",
-  "^svante *(n(elander)?)? *$": "$ Karl, Malin, Bonx, Bånx och påven",
+  "^olof *(h(ylander)?)? *$": "$ Tove.",
+  "^tove *(l(eander)?)? *(r(önngren)?)? *$": "$ Svante.",
+  "^svante *(n(elander)?)? *$": "$ Jingrui.",
+  "^jingrui *(g(uo)?)? *$": "$ Isabella.",
+  "^isabella *(d'?arcy)? *$": "$ Matilda.",
+  "^matilda *(k(ar(e|é|è)n)?)? *$": "$ Sinit.",
+  "^sinit *(t(esfamichael)?)? *(f(eshaye)?)? *$": "$ Malin.",
+  "^malin *((a|å)(sman)?)? *$": "$ Elly.",
+  "^elly *(g(illgren)?)? *$": "$ Tamara.",
+  "^tamara *(h(anifi)?)? *$": "$ Josephine.",
+  "^josephine *(j(atzkowski)?)? *$": "$ Elsa.",
+  "^elsa *(n(ordlund)?)? *$": "$ Stella.",
+  "^stella *(a(fzelius)?)? *$": "$ Setayesh.",
+  "^setayesh *(g(arousi)?)? *$": "$ Arvid S.",
+  "^arvid *s(öderberg)? *$": "$ Emil.",
+  "^emil *(l(indström)?)? *$": "$ Ellie.",
+  "^ellie *(e(riksson)?)? *$": "$ Hannes.",
+  "^hannes *(e(riksson)?)? *$": "$ Ines.",
+  "^ines *((å|a)(sberg)?)? *$": "$ Olof.",
 };
 
 // Blanda en lista, använd när deltagare ska få målpersoner
@@ -92,3 +121,9 @@ function shuffleList(list) {
   }
   return out;
 }
+
+/*
+
+'olof', 'tove', 'svante', 'Jingrui', 'isabella', 'Matilda', 'Sinit', 'malin', 'elly', 'tamara', 'Josephine', 'Elsa', 'stella', 'setayesh', 'arvid S', 'emil', 'Ellie', 'hannes', 'ines'
+
+*/
